9f80a1aa9f6bcec48e06e780a2e07de8f66c50dc
Akhil Sabu <kingasv@gmail.com>
2016-07-16 14:48:19 +0530

    a
    a

---
+++ config.js
@@ -0,0 +1,6 @@
+var secret_key = "asssss";
+var map_api_key="AIzaSyBoL26hVGudbJBDMo5yT3PIc-A93EX67NE";
+var server_domain = 'http://103.53.43.14/asv/Source/';
+//var server_domain = 'http://192.168.1.31/shajeer/cmc_website/Source';
+//var server_domain = 'http://192.168.138.31/lidiya/callmycab';
+var wordpress = false;
